c7ea08b53c03b2a4076336efad27dd4ac6b92c33
Brad Garropy <bradgarropy@gmail.com>
2019-06-03 15:42:09 -0500

    add withAssetRelocator.

--- next.config.js
+++ next.config.js
@@ -1,6 +1,42 @@
 require("dotenv").config()
 
-module.exports = {
+// hack for firebase
+// https://github.com/zeit/next.js/issues/6073
+const withAssetRelocator = (nextConfig = {}) => {
+    return Object.assign({}, nextConfig, {
+        webpack(config, options) {
+            const {isServer} = options
+
+            if (isServer) {
+                config.node = Object.assign({}, config.node, {
+                    __dirname: false,
+                    __filename: false,
+                })
+
+                config.module.rules.unshift({
+                    test: /\.(m?js|node)$/,
+                    parser: {amd: false},
+                    use: {
+                        loader: "@zeit/webpack-asset-relocator-loader",
+                        options: {
+                            outputAssetBase: "assets",
+                            existingAssetNames: [],
+                            wrapperCompatibility: true,
+                            escapeNonAnalyzableRequires: true,
+                        },
+                    },
+                })
+            }
+
+            if (typeof nextConfig.webpack === "function") {
+                return nextConfig.webpack(config, options)
+            }
+            return config
+        },
+    })
+}
+
+const config = {
     target: "serverless",
     env: {
         firebase: {
@@ -14,3 +50,5 @@ module.exports = {
         },
     },
 }
+
+module.exports = withAssetRelocator(config)

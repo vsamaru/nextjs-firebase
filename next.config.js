6e8c79333b3c2d11bf4add0a6c3106ac73655c3e
Brad Garropy <bradgarropy@gmail.com>
2019-06-03 23:24:38 -0500

    remove workaround.

--- next.config.js
+++ next.config.js
@@ -1,41 +1,5 @@
 require("dotenv").config()
 
-// hack for firebase
-// https://github.com/zeit/next.js/issues/6073
-const withAssetRelocator = (nextConfig = {}) => {
-    return Object.assign({}, nextConfig, {
-        webpack(config, options) {
-            const {isServer} = options
-
-            if (isServer) {
-                config.node = Object.assign({}, config.node, {
-                    __dirname: false,
-                    __filename: false,
-                })
-
-                config.module.rules.unshift({
-                    test: /\.(m?js|node)$/,
-                    parser: {amd: false},
-                    use: {
-                        loader: "@zeit/webpack-asset-relocator-loader",
-                        options: {
-                            outputAssetBase: "assets",
-                            existingAssetNames: [],
-                            wrapperCompatibility: true,
-                            escapeNonAnalyzableRequires: true,
-                        },
-                    },
-                })
-            }
-
-            if (typeof nextConfig.webpack === "function") {
-                return nextConfig.webpack(config, options)
-            }
-            return config
-        },
-    })
-}
-
 const config = {
     target: "serverless",
     env: {
@@ -51,4 +15,4 @@ const config = {
     },
 }
 
-module.exports = withAssetRelocator(config)
+module.exports = config

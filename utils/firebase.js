2c3b3b148040d7ea73fe61a98af166c0e0ccec98
Brad Garropy <bradgarropy@gmail.com>
2019-06-05 23:09:55 -0500

    log project id.

--- utils/firebase.js
+++ utils/firebase.js
@@ -2,6 +2,8 @@ import firebase from "firebase/app"
 import "firebase/auth"
 import "firebase/firestore"
 
+console.log(process.env.FIREBASE_PROJECT_ID)
+
 const config = {
     apiKey: process.env.FIREBASE_API_KEY,
     authDomain: `${process.env.FIREBASE_PROJECT_ID}.firebaseapp.com`,

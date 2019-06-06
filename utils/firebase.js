242ca4cd1754ff7a8a273b19579798fe6eeb07e8
Brad Garropy <bradgarropy@gmail.com>
2019-06-05 22:59:56 -0500

    try workaround with webpack asset reloader.

--- utils/firebase.js
+++ utils/firebase.js
@@ -1,23 +1,21 @@
-import * as Firebase from "firebase/app"
+import firebase from "firebase/app"
+import "firebase/auth"
 import "firebase/firestore"
 
 const config = {
-    apiKey: process.env.firebase.apiKey,
-    authDomain: process.env.firebase.authDomain,
-    databaseURL: process.env.firebase.databaseURL,
-    projectId: process.env.firebase.projectId,
-    storageBucket: process.env.firebase.storageBucket,
-    messagingSenderId: process.env.firebase.messagingSenderId,
-    appId: process.env.firebase.appId,
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: `${process.env.FIREBASE_PROJECT_ID}.firebaseapp.com`,
+    databaseURL: `https://${process.env.FIREBASE_PROJECT_ID}.firebaseio.com`,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: `${process.env.FIREBASE_PROJECT_ID}.appspot.com`,
+    messagingSenderId: process.env.FIREBASE_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID,
 }
 
-const firebase = Firebase.apps.length
-    ? Firebase.app()
-    : Firebase.initializeApp(config)
+const app = firebase.apps.length
+    ? firebase.app()
+    : firebase.initializeApp(config)
+const auth = firebase.auth()
+const firestore = firebase.firestore()
 
-const firestore = Firebase.firestore()
-
-module.exports = {
-    firebase,
-    firestore,
-}
+export {firebase, app, auth, firestore}

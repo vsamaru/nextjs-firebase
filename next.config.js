2ea121b194b20c034c4bd83c25aea6b5be923fa7
Brad Garropy <bradgarropy@gmail.com>
2019-06-07 11:22:09 -0500

    config files.

--- next.config.js
+++ next.config.js
@@ -1,52 +1,54 @@
-require('dotenv').config()
+require("dotenv").config({
+    path: ".env.build",
+})
 
 const withAssetRelocator = (nextConfig = {}) => {
-  return Object.assign({}, nextConfig, {
-    webpack(config, options) {
-      const { isServer } = options
+    return Object.assign({}, nextConfig, {
+        webpack(config, options) {
+            const {isServer} = options
 
-      if (isServer) {
-        config.node = Object.assign({}, config.node, {
-          __dirname: false,
-          __filename: false
-        })
+            if (isServer) {
+                config.node = Object.assign({}, config.node, {
+                    __dirname: false,
+                    __filename: false,
+                })
 
-        config.module.rules.unshift({
-          test: /\.(m?js|node)$/,
-          parser: { amd: false },
-          use: {
-            loader: '@zeit/webpack-asset-relocator-loader',
-            options: {
-              outputAssetBase: 'assets',
-              existingAssetNames: [],
-              wrapperCompatibility: true,
-              production: true
+                config.module.rules.unshift({
+                    test: /\.(m?js|node)$/,
+                    parser: {amd: false},
+                    use: {
+                        loader: "@zeit/webpack-asset-relocator-loader",
+                        options: {
+                            outputAssetBase: "assets",
+                            existingAssetNames: [],
+                            wrapperCompatibility: true,
+                            production: true,
+                        },
+                    },
+                })
             }
-          }
-        })
-      }
 
-      if (typeof nextConfig.webpack === 'function') {
-        return nextConfig.webpack(config, options)
-      }
-      return config
-    }
-  })
+            if (typeof nextConfig.webpack === "function") {
+                return nextConfig.webpack(config, options)
+            }
+            return config
+        },
+    })
 }
 
 const config = {
-  target: 'serverless',
-  env: {
-    firebase: {
-      apiKey: process.env.FIREBASE_API_KEY,
-      authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-      databaseURL: process.env.FIREBASE_DATABASE_URL,
-      projectId: process.env.FIREBASE_PROJECT_ID,
-      storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-      messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-      appId: process.env.FIREBASE_APP_ID
-    }
-  }
+    target: "serverless",
+    env: {
+        firebase: {
+            apiKey: process.env.FIREBASE_API_KEY,
+            authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+            databaseURL: process.env.FIREBASE_DATABASE_URL,
+            projectId: process.env.FIREBASE_PROJECT_ID,
+            storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+            messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+            appId: process.env.FIREBASE_APP_ID,
+        },
+    },
 }
 
 module.exports = withAssetRelocator(config)
